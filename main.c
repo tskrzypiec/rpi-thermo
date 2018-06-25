94bcb3fda79476bc0ff910543b88a02763d52a78
tskrzypiec <tomasz.t.skrzypiec@gmail.com>
2018-06-25 23:58:15 +0200

    ver.1

--- main.c
+++ main.c
@@ -4,30 +4,21 @@
 #include <curl/curl.h>
 #include <wiringPi.h>
 #include <stdint.h>
-
+#include <ctype.h>
 #define URL_SIZE_MAX 120
-
 static void post_event(char *url);
-
+float curr_temp = 0;
 #define MAXTIMINGS      85
 #define DHTPIN          7
 int dht11_dat[5] = {0, 0, 0, 0, 0};
 
 
-char *socket_eft_on = "https://maker.ifttt.com/trigger/gniazdko_on/with/key/";
-char *socket_evt_off = "https://maker.ifttt.com/trigger/gniazdko_off/with/key/";
-
-// build full url with the secret key
-char full_url_socket_on[URL_SIZE_MAX] = {0};
-char full_url_socket_off[URL_SIZE_MAX] = {0};
-//  snprintf(full_url_socket_on, sizeof(full_url_socket_on), "%s%s", socket_evt_on));
-//   snprintf(full_url_socket_off, sizeof(full_url_socket_off), "%s%s", socket_evt_off, getenv("KEY"));
-
-void read_dht11_dat() {
+float read_dht11_dat(void)
+{
+    static float result = 0.0f;
     uint8_t laststate = HIGH;
     uint8_t counter = 0;
     uint8_t j = 0, i;
-    float f; /* fahrenheit */
 
     dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
 
@@ -42,7 +33,8 @@ void read_dht11_dat() {
     pinMode(DHTPIN, INPUT);
 
     /* detect change and read data */
-    for (i = 0; i < MAXTIMINGS; i++) {
+    for (i = 0; i < MAXTIMINGS; i++)
+    {
         counter = 0;
         while (digitalRead(DHTPIN) == laststate) {
             counter++;
@@ -57,7 +49,8 @@ void read_dht11_dat() {
             break;
 
         /* ignore first 3 transitions */
-        if ((i >= 4) && (i % 2 == 0)) {
+        if ((i >= 4) && (i % 2 == 0))
+        {
             /* shove each bit into the storage bytes */
             dht11_dat[j / 8] <<= 1;
             if (counter > 16)
@@ -65,39 +58,30 @@ void read_dht11_dat() {
             j++;
         }
     }
+
     /*
      * check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
-printf( "Temperature = %d.%d *C (%.1f *F)\n",
-                     dht11_dat[2], dht11_dat[3], f );        * print it out if data is good
+        printf( "Temperature = %d.%d *C (%.1f *F)\n",
+        dht11_dat[2], dht11_dat[3], f );
+        * print it out if data is good
      */
     if ((j >= 40) &&
-        (dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF))) {
-        f = dht11_dat[2] * 9. / 5. + 32;
-        printf("Temperature = %d.%d *C (%.1f *F)\n",
-               dht11_dat[2], dht11_dat[3], f);
-
-    } else {
-        printf("Data not good, skip\n");
+        (dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF)))
+    {
+        printf("Temperature = %d.%d *C\n", dht11_dat[2], dht11_dat[3]);
+        result = (float) dht11_dat[2]; /* todoa: add fract part to the result */
+    }
+    else
+    {
+        //printf("dbg: Data not good, skip\n"); /* uncomnent for debug */
+        // data is not good, keep precious result
     }
 
-    float curr_temp = (float) dht11_dat[2] + (float) dht11_dat[3] / 100;
-    printf("test: %f\n", curr_temp);
-    float czujnik = 0;
-
-    czujnik = curr_temp;
-/*                              if(curr_temp>25)
-                        {
-                                        post_event(full_url_socket_on);
-
-                                }
-else{
-printf("za niska temperatura!\n");
-}
-*/
-
+    return result;
 }
 
-static void post_event(char *url) {
+static void post_event(char *url)
+{
     CURL *curl = curl_easy_init();
     CURLcode curl_res = CURLE_FAILED_INIT;
 
@@ -118,48 +102,52 @@ static void post_event(char *url) {
     }
 }
 
-
-int main(void) {
-
-    float curr_temp = 0;
-    float czujnik;
-    // test getenv
-    // to set env in command line type: export VAR="VAL"
-    //printf("KEY = %s\n", getenv("KEY"));
-
-
+int main(int argc, char **argv)
+{
     if (wiringPiSetup() == -1)
+    {
         exit(1);
+    }
 
-    while (1) {
-        read_dht11_dat();
-        delay(1000); /* wait 1sec to refresh */
+    // handle command line args
+    if (1 > argc)
+    {
+          std::cerr << "Usage: " << name << " <option>\n"
+              << "Options:\n"
+              << "\t--[number_of_degress_you_want_to_start_coolig_from]\n"
+              << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
+    int config_arg = strtol(argv[1], NULL, 10);
+    printf("dbg arg: %d\n", config_arg);
 
+    // build full url with the secret key
+    char *socket_evt_on = "https://maker.ifttt.com/trigger/gniazdko_on/with/key/";
+    char *socket_evt_off = "https://maker.ifttt.com/trigger/gniazdko_off/with/key/";
+    char full_url_socket_on[URL_SIZE_MAX] = {0};
+    char full_url_socket_off[URL_SIZE_MAX] = {0};
+    snprintf(full_url_socket_on, sizeof(full_url_socket_on), "%s%s", socket_evt_on, getenv("KEY"));
+    snprintf(full_url_socket_off, sizeof(full_url_socket_off), "%s%s", socket_evt_off, getenv("KEY"));
 
-        if (curr_temp> 25) {
+    // main program
+    float read_temp = 0.0f;
+    float temp_down = 5.0f; /* magic number */
+
+    while (1)
+    {
+        read_temp = read_dht11_dat();
+
+        if (config_arg < read_temp)
+        {
             post_event(full_url_socket_on);
+        }
 
-        } else {
-
-            printf("za niska temperatura!\n, bo jest =");
-            printf("%f", czujnik);
+        if (((float)config_arg-temp_down) > read_temp)
+        {
+            post_event(full_url_socket_off);
         }
     }
 
-    /* if(curr_temp>25)
-       {
-          post_event(full_url_socket_on);
-
-       }
-    else{
-
-    printf("za niska temperatura!\n");
-
-    }*/
-
-
-//post_event(fulurl_socket_on);
-
-    return (0);
+    return 0;
 }
